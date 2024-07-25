22ebc4f363d1a3afe65a1a23e7c84dbdd1fa13b9
Muhammad Ali <67823606+muhammadali5318@users.noreply.github.com>
2024-07-25 10:24:35 -0700

    Time: 47 ms (88.49%) | Memory: 48.6 MB (95.66%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.js
+++ 1894-merge-strings-alternately/merge-strings-alternately.js
@@ -3,17 +3,21 @@
  * @param {string} word2
  * @return {string}
  */
-var mergeAlternately = function(word1, word2) {
-    let merged = "";
-    let i = 0, j = 0;
+var mergeAlternately = function (word1, word2) {
+   let l1 = word1.length;
+    let l2 = word2.length;
+    let loopSize = Math.min(l1, l2);
+    let sumOfString = "";
 
-    while (i < word1.length && j < word2.length) {
-        merged += word1[i] + word2[j];
-        i++;
-        j++;
+    for (let i = 0; i < loopSize; i++) {
+        sumOfString += word1[i] + word2[i];
     }
-
-    merged += word1.slice(i) + word2.slice(j);
-
-    return merged;
+    
+    if (l1 > l2) {
+        sumOfString += word1.substring(loopSize);
+    } else {
+        sumOfString += word2.substring(loopSize);
+    }
+    
+    return sumOfString;
 };
